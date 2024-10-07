add5dc9c565e0dce775c5c25551f1a17ff7c449f
unknown <manarshblah@gmail.com>
2024-10-07 13:52:21 +0300

    first commit

---
+++ translation/translation.js
@@ -0,0 +1,188 @@
+
+const translations = {
+  en: {
+    'text-header1': 'AZYAE HAYEL',
+    'text-header2': 'For Readymade Garments Trading',
+    'Home': 'Home',
+    'Men': " Men's",
+    'Women': "Women's",
+    'Kids': "Kids's",
+    "Family": "Family",
+    'Pages': "Pages",
+    'AboutUs': "AboutUs",
+    'ContactUs': "Contact Us",
+    'English': "English",
+    'arabic': "arabic",
+    'AboutOurCompany': "About Our Company",
+    'dis': "AZYAE HAYEL FOR READYMADE GARMENTS TRADING",
+    'dis1': "In our company, we design all types of fashion in all different colors and sizes",
+    'dis2': "Here you will find all the popular and modern models. Here you will find what you are looking for or looking for",
+    'dis3': "Our company offers many good and distinctive offers, especially if you are one of our valued customers. We will be very happy if you take a look at our products.",
+    'Subscribing': "By Subscribing To Our Newsletter You Can Get 30% Off",
+    'Phone': "Phone :<br><span>+971-523435020 </span>",
+    'OfficeLocation': "OfficeLocation :<br><span >Dubai – Bur Dubai – Al Fahidi</span>",
+    'Email': "Email:<br><span>ertyuiopertyuiop111@gmail.com</span>",
+    'SocialMedia': "Social Media:",
+    'Facebook': "Facebook:",
+    'Instagram': "Instagram:",
+    "hero-1": "We Are AZYAE HAYEL",
+    "hero-2": "Best Clothes For Women",
+    "hero-3": "Unleash your confidence. Trendy fashion for the modern woman",
+    "hero-4": "Best Clothes For Men",
+    "hero-5": "Elevate your style. Premium quality menswear for every occasion. ",
+    "hero-6": "Best Clothes For Kids",
+    "hero-8": "Playful and comfy. Adorable outfits for your little ones.",
+    "hero-9": "for every one in your family",
+    "Discover": "Discover More",
+
+    "Jackets": "Jackets",
+    "jeans": "jeans",
+    "men-1": "Men's Latest",
+    "Shirts": "Shirts",
+    "T-Shirts": "T-Shirts",
+    "Footwear": "Footwear",
+    "Activewear": "Activewear",
+    "Dresses": "Dresses",
+    "Skirts":"Skirts",
+    "Tops": "Tops",
+    "ClassicSpring": "Classic Spring",
+    "Hoodies": "Hoodies",
+    "PantsandShorts": "Pants and Shorts", 
+    "Men’s Shopping": "Men’s Shopping",
+    "Women’s Shopping": "Women’s Shopping",
+    "Kid's Shopping": "Kid's Shopping",
+
+    "men-2": "you can find everything you want here in men's fashion",
+
+    "women-1": "Women's Latest",
+    "women-2": "you can find everything you want here in women's fashion",
+
+    "kids-1": "Kid's Latest",
+    "kids-2": "you can find everything you want here in kids fashion",
+
+    "SEE": "SEE MORE",
+
+    "footer-1": "by your fashion you can be most trinding in media",
+    "Fashion": "Fashion",
+    "New": "New",
+    "Brand": "Brand",
+    "Makeup": "Makeup",
+    "Leather": "Leather",
+    "Bag": "Bag",
+    "text": "For Readymade Garments Trading",
+    "city": "Dubai – Bur Dubai – Al Fahidi",
+    "shop": "Shopping &amp; Categories",
+    "Links": "Useful Links",
+    "Say": "Say Hello. Don't Be Shy!",
+    "details": " 1.Size: L<hr>2. Style and Silhouette:Classic<hr>3. Fabric and Comfort:cotton<hr>4.Prise :100$",
+
+    "check":"Check Our Products",
+    "h-Kids":"Our Latest Products For Kids",
+    "h-Kids1":"Check out all of our products",
+    "h-men": "Our Latest Products For Men",
+    "h-women":"Our Latest Products For Women",
+    // Add more translations here
+  },
+  ar: {
+    'text-header1': 'أزياء حايل',
+    'text-header2': 'لتجارة الملابس الجاهزة',
+    'Home': 'الصفحة الرئيسية',
+    'Men': 'رجال',
+    'Women': 'نساء',
+    'Kids': 'اطفال',
+    'Pages': 'صفحات',
+    'AboutUs': "من نحن",
+    'ContactUs': "تواصل معنا",
+    'English': "الانجيزية",
+    'arabic': "العربية",
+    'AboutOurCompany': " شركتنا",
+    'dis': "أزاي هايل لتجارة الملابس الجاهزة",
+    'dis1': "نحن في شركتنا نصمم كل انواع الموضة بكل الالوان والقياسات المتنوعة",
+    'dis2': "حيث هنا ستجد كل الموديلات الرائجة والعصرية.هنا ستجد ماتطلبه او تبحث عنه",
+    'dis3': "شركتنا تقدم الكثير من العروض الجيدة والمميزة وخصوصا ان كنت من زبائننا الكرام سنكون سعيدين جدا لو القيت نظرة على منتجاتنا",
+    'Subscribing': "بالاشتراك في نشرتنا الإخبارية يمكنك الحصول على خصم 30%",
+    'Phone': " الهاتف :<br><span>+971-523435020 </span>",
+    'OfficeLocation': "الموقع :<br><span>دبي – بر دبي – الفهيدي</span>",
+    'Email': "الايميل:<br><span>ertyuiopertyuiop111@gmail.com</span>",
+    'SocialMedia': " التواصل:",
+    'Facebook': "فيسبوك:",
+    'Instagram': "انستغرام:",
+    "hero-1": "أزياء أهيل",
+    "hero-2": "أفضل الملابس النسائية",
+    "hero-3": "أطلقي العنان لثقتك الموضة العصرية.",
+    "Discover": "المزيد",
+    "hero-4": "أفضل الملابس الرجالية",
+    "hero-5": "ارفع من أسلوبك ملابس رجالية عالية الجودة للمناسبات",
+    "hero-6": "أفضل ملابس الأطفال",
+    "hero-8": "ملابس رائعة لأطفالك الصغار",
+    "hero-9": "من أجل كل فرد في العائلة",
+    "Family": "العائلة",
+    "men-1": "الموضة الرجالية",
+    "men-2": "يمكنك أن تجد هنا كل شيئ تريده",
+    "Jackets": "جاكيت",
+    "jeans": "جينز",
+    "Shirts": "قميص",
+    "T-Shirts": "قميص",
+    "Footwear": "أحذية",
+    "SEE": "رؤية المزيد",
+    "women-1": "الموضة النسائية",
+    "women-2": "يمكنك أن تجدي هنا كل شيئ تريدينه",
+    "Activewear": "ملابس رياضية",
+    "Dresses": "فساتين",
+    "Skirts":"التنانير",
+    "Tops": "توبس",
+    "ClassicSpring": "القسم الربيعي",
+    "kids-1": "موضة الأطفال",
+    "kids-2": "يمكنك أن تجد هنا كل ما تريد من ملايس للأطفال",
+    "Hoodies": "هوديس",
+    "PantsandShorts": "بناطلين وشورت", 
+    "shop": "تسوق  &amp; الفئات",
+    "Men’s Shopping": "الرجال",
+    "Women’s Shopping": "النساء",
+    "Kid's Shopping": "الأطفال",
+    "Links": "الصفحات",
+    "check":"تصفح منتجاتنا",
+    "h-Kids":"أخر تصاميمنا للأطفال",
+    "h-Kids1":"تصفح كافة منتجاتنا",
+    "details": " 1.القياس: L<hr>2. الموديل :كلاسيك<hr>3.نوع القماش:قطن<hr>4.السعر :100$",
+
+    "h-men":"أخر تصاميمنا للرجال",
+    "h-women":"أخر تصاميمنا للنساء",
+    "Say": "قل مرحبا. لا تكن خجولا",
+    "": "",
+    "": "",
+
+    // Add more translations here
+  },
+};
+
+const defaultLanguage = 'en'; // Set the default language
+let currentLanguage = defaultLanguage; // Initialize the current language
+
+
+function translatePage() {
+  const elements = document.querySelectorAll('[data-translate]');
+  elements.forEach((element) => {
+    const translationKey = element.getAttribute('data-translate');
+    element.innerHTML = translations[currentLanguage][translationKey];
+  });
+}
+
+
+
+function changeLanguage(language) {
+  currentLanguage = language;
+  translatePage();
+}
+
+// Example of how to change the language:
+document.getElementById('english-button').addEventListener('click', () => {
+  changeLanguage('en');
+});
+
+document.getElementById('arabic-button').addEventListener('click', () => {
+  changeLanguage('ar');
+});
+
+// Initially translate the page to the default language
+translatePage();
